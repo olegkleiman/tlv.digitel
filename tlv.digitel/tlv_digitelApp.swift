c6ed68512b490856325cefcade412532d6ed3753
Oleg Kleiman <oleg_kleyman@hotmail.com>
2022-07-10 17:55:02 +0300

    Initial Commit

---
+++ tlv.digitel/tlv_digitelApp.swift
@@ -0,0 +1,17 @@
+//
+//  tlv_digitelApp.swift
+//  tlv.digitel
+//
+//  Created by Oleg Kleiman on 10/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct tlv_digitelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
